cde702e0fea59d6aae0d36851d994e4896ac9372
Eael Work <eaelspagad@outlook.com>
2024-07-10 03:57:08 +0000

    adding forbidden error handler and route

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,9 +22,15 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def not_found(error) -> str:
-    """Unauthorized Handler
+    """ Unauthorized Handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+@app.errorhandler(403)
+def not_found(error) -> str:
+    """ Forbidden Handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
