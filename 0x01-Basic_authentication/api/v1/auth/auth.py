fdf7672a7c331644ae18f64282fea1895eb33258
Eael Work <eaelspagad@outlook.com>
2024-07-10 04:36:53 +0000

    basic_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -27,6 +27,8 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """Gets authorization header from request
         """
+        if request is not None:
+            return request.headers.get('Authorization', 'None')
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
